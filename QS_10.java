d01d74159d0a081429869b05158c6aed6b1cec9c
Soumita Das <soumitad991@gmail.com>
2024-08-06 20:10:18 +0530

    Increment&DecrementOperators

---
+++ QS_10.java
@@ -0,0 +1,9 @@
+public class QS_10 {
+    public static void main(String[] args){
+        int i=0;
+        int x=i++; //0,1
+        int y=++i; 
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
